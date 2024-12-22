214736c5eeab9dd7f2295dceb665e5391357719c
Pritam Ghosh <44809987+meghoshpritam@users.noreply.github.com>
2024-12-22 22:43:26 +0530

    Add initial project setup with global styles, utility functions, and UI components

---
+++ src/components/icons/InfiniteLoaderIcon.tsx
@@ -0,0 +1,27 @@
+import type React from "react";
+
+const InfiniteLoaderIcon: React.FC<React.SVGProps<SVGElement>> = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 150" {...props}>
+        <title>Infinite Loader Icon</title>
+        <path
+            fill="none"
+            stroke="#FF156D"
+            strokeDasharray="300 385"
+            strokeLinecap="round"
+            strokeWidth="15"
+            d="M275 75c0 31-27 50-50 50-58 0-92-100-150-100-28 0-50 22-50 50s23 50 50 50c58 0 92-100 150-100 24 0 50 19 50 50Z"
+            style={{ "--darkreader-inline-stroke": "#f96ef9" }}
+        >
+            <animate
+                attributeName="stroke-dashoffset"
+                calcMode="spline"
+                dur="2"
+                keySplines="0 0 1 1"
+                repeatCount="indefinite"
+                values="685;-685"
+            />
+        </path>
+    </svg>
+);
+
+export default InfiniteLoaderIcon;
---
+++ src/helpers/downloadFont.helper.ts
@@ -0,0 +1,95 @@
+import axios from "axios";
+import { saveAs } from "file-saver";
+import JsZip from "jszip";
+
+export const downloadFontZip = async (css: string, fontSrcPrefix: string) => {
+    const fontFaceSrcRegex = /src:\s*url\(([^)]*)\)/g;
+    const fontFaceFontFamilyRegex = /font-family:\s*([^;]*)/g;
+    const fontFaceFontWeightRegex = /font-weight:\s*([^;]*)/g;
+    const fontFaceFontStyleRegex = /font-style:\s*([^;]*)/g;
+
+    const lines = css.split("\n");
+    const linksToDownload = [];
+
+    let characterSet: string;
+    let fonStyle: string;
+    let fontWeight: string;
+    let src: string;
+    let frontFamily: string;
+
+    for (const lineRaw of lines) {
+        const line = lineRaw.trim();
+        if (line.startsWith("/*") && line.endsWith("*/")) {
+            characterSet =
+                line?.replace("/*", "")?.replace("*/", "").trim() || "";
+        }
+        if (line.startsWith("font-style:")) {
+            fonStyle =
+                line
+                    ?.match(fontFaceFontStyleRegex)?.[0]
+                    ?.replace("font-style:", "")
+                    ?.trim() || "";
+        }
+        if (line.startsWith("font-weight:")) {
+            fontWeight =
+                line
+                    ?.match(fontFaceFontWeightRegex)?.[0]
+                    ?.replace("font-weight:", "")
+                    ?.trim() || "";
+        }
+        if (line.startsWith("font-family:")) {
+            frontFamily =
+                line
+                    ?.match(fontFaceFontFamilyRegex)?.[0]
+                    ?.replace("font-family:", "")
+                    ?.trim() || "";
+        }
+        if (line.startsWith("src:")) {
+            src =
+                line
+                    ?.match(fontFaceSrcRegex)?.[0]
+                    ?.replace("src:", "")
+                    ?.replace("url(", "")
+                    ?.replace(")", "")
+                    ?.trim() || "";
+
+            const format = src?.split(".")?.pop();
+
+            if (src && format && frontFamily && fonStyle && fontWeight) {
+                linksToDownload.push({
+                    src,
+                    fileName:
+                        `${frontFamily}-${characterSet}-${fonStyle}-${fontWeight}.${format}`
+                            .replace(/['"]/gm, "")
+                            .toLowerCase(),
+                });
+            }
+        }
+    }
+
+    let updatedCss = css;
+    for (const { src, fileName } of linksToDownload) {
+        updatedCss = updatedCss.replace(src, `'${fontSrcPrefix}${fileName}'`);
+    }
+    const responses = await Promise.all(
+        linksToDownload.map(async ({ src, fileName }) => {
+            const res = await axios.get(src, { responseType: "blob" });
+
+            return {
+                fileName,
+                blob: res.data,
+            };
+        }),
+    );
+
+    const zip = new JsZip();
+    for (const { fileName, blob } of responses) {
+        zip.file(fileName, blob);
+    }
+
+    zip.file("font.css", updatedCss);
+
+    const zipBlob = await zip.generateAsync({ type: "blob" });
+    saveAs(zipBlob, "font-files.zip");
+    console.log("🚀[downloadFont.helper.ts:57]: responses: ", responses);
+};
